9d10128d859398c40848f367a9bb4fdf8e755281
Huw Jones <huwjones025@gmail.com>
2019-09-11 14:25:46 +0100

    develops basic basket

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import {ContextConsumer} from '../Context'
+import { ContextConsumer } from '../Context'
+import { IProductImages, IProduct } from './interfaces'
 
 const Basket: React.FC = () => {
   return (
     <ContextConsumer>
       {context => {
         const [basket, updateBasket] = context
-        console.log(basket)
-        return <div>
-          <button onClick={() => {updateBasket((old: any) => {
-            return [...old, 'b']
-          })}}></button>
-        </div>
+        if (!basket.length) return <h3>Nothing in your basket!</h3>
+        const name: string = basket[0].name
+        const price: number = basket[0].price
+        const images: IProductImages[] = basket[0].images
+
+        return (
+          <main>
+            <h1>Shopping Basket</h1>
+            <article>
+              <img src={images[0].src} alt="the product in the basket" />
+              <h1>{name}</h1>
+              <h3>Quantity:</h3>
+              <button onClick={() => {
+                updateBasket((prevBasket: IProduct[]) => {
+                  return prevBasket.slice(0, -1)
+                })
+              }}>-</button>
+              <h3>{basket.length}</h3>
+              <button onClick={() => {
+                updateBasket((prevBasket: IProduct[]) => {
+                  return [...prevBasket, prevBasket[0]]
+                })
+              }}>+</button>
+              <h2>Total: £{basket.length * price}</h2>
+              <button onClick={() => {
+                updateBasket(() => {
+                  return []
+                })
+              }}>Remove</button>
+            </article>
+          </main>)
       }}
     </ContextConsumer>
   )
