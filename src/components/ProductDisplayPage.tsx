a81ff1f5555a6b1710653a6df66b957576932a0b
Huw Jones <huwjones025@gmail.com>
2019-09-11 14:04:12 +0100

    creates basic content for PDP

--- src/components/ProductDisplayPage.tsx
+++ src/components/ProductDisplayPage.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { IProductImages, IProduct } from './interfaces';
+import products from './products';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { ContextConsumer } from '../Context'
 
 const ProductDisplayPage: React.FC = () => {
+  const name: string = products[0].name
+  const description: string = products[0].description
+  const price: number = products[0].price
+  const images: IProductImages[] = products[0].images
+
   return (
-    <h1>pdp</h1>
+    <article>
+      <div style={{ width: "40vw" }}>
+        <Carousel>
+          {images.map(img => (
+            <div>
+              <img src={img.src} alt={img.description} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <h1>{name}</h1>
+      <p>{description}</p>
+      <ContextConsumer>
+        {context => {
+          const [basket, updateBasket] = context
+          return <div>
+            <button onClick={() => {
+              updateBasket((prevBasket: IProduct[]) => {
+                return [...prevBasket, products[0]]
+              })
+            }}>
+              Add To Basket
+            </button>
+          </div>
+        }}
+      </ContextConsumer>
+      <h2>£{price}</h2>
+    </article>
   );
 }
 
