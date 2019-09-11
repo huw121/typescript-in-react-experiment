343328d2287483fcd0315b78a1024e678ffcc058
Huw Jones <huwjones025@gmail.com>
2019-09-11 17:17:50 +0100

    adds global style from styled component for body

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,33 @@ import ProductDisplayPage from './components/ProductDisplayPage';
 import Basket from './components/Basket';
 import Nav from './components/Nav';
 import { ContextProvider } from './Context';
-import './App.css';
 import RouteNotFound from './components/RouteNotFound'
+import { createGlobalStyle } from 'styled-components'
+
+const BodyStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #c20e53;
+    font-family: monospace;
+    color: white
+  }
+`
 
 const App: React.FC = () => {
   const [basket, updateBasket] = useState([]);
   return (
-      <ContextProvider value={[basket, updateBasket]}>
-        <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route path="/" exact component={ProductDisplayPage} />
-            <Route path="/basket" component={Basket} />
-            <RouteNotFound />
-          </Switch>
-        </BrowserRouter>
-      </ContextProvider>
+    <ContextProvider value={[basket, updateBasket]}>
+      <BodyStyle />
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={ProductDisplayPage} />
+          <Route path="/basket" component={Basket} />
+          <RouteNotFound />
+        </Switch>
+      </BrowserRouter>
+    </ContextProvider>
   );
 }
 
