8b9f6e0ff0c9b3063296c94e30331a526a2a2897
Huw Jones <huwjones025@gmail.com>
2019-09-11 15:33:42 +0100

    adds styling to PDP

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
-const NavList = styled.nav`
-  list-style-type: none;
-  display: flex;
-  justify-content: space-around
-`
-
-const NavButton = styled(Link)`
-  display: block;
-  padding: 1.2em;
-  text-decoration: none;
-  color: white;
-  font-size: 1.6em;
-  font-weight: bold;
-  &:hover {
-    color: #c20e53;
-    background-color: white;
-  }
-`
-
 const Nav: React.FC = () => {
+
+  const NavList = styled.nav`
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around
+    background-color: #b20e53;
+  `
+  
+  const NavButton = styled(Link)`
+    display: block;
+    padding: 1.2em;
+    text-decoration: none;
+    color: white;
+    font-size: 1.6em;
+    font-weight: bold;
+    &:hover {
+      color: #c20e53;
+      background-color: white;
+    }
+  `
+
   return (
       <NavList>
         <NavButton to="/">Product</NavButton>
--- src/components/ProductDisplayPage.tsx
+++ src/components/ProductDisplayPage.tsx
@@ -3,17 +3,53 @@ import { IProductImages, IProduct } from './interfaces';
 import products from './products';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import { ContextConsumer } from '../Context'
+import { ContextConsumer } from '../Context';
+import styled from 'styled-components';
 
 const ProductDisplayPage: React.FC = () => {
+
+  const Product = styled.article`
+    display: flex;
+    margin: 1em
+    @media (max-width: 768px) {
+      flex-direction: column;
+    }
+  `
+  const ImageContainer = styled.div`
+    padding: 1em;
+  `
+  const ProductInfo = styled.div`
+    padding: 1em;
+    display: flex;
+    flex-direction: column;
+  `
+  const ImportantInfo = styled.h1`
+    font-weight: bold;
+    font-size: 2.5em;
+  `
+  const Description = styled.p`
+    font-size: 1.3em;
+  `
+  const AddToBasket = styled.button`
+    font-size: 1.3em;
+    font-weight: bold;
+    padding: 0.25em 1em;
+    border: 1px solid #b20e53;
+    background: white;
+    color: #b20e53;
+    &:hover {
+      cursor: pointer;
+    }
+  `
+
   const name: string = products[0].name
   const description: string = products[0].description
   const price: number = products[0].price
   const images: IProductImages[] = products[0].images
 
   return (
-    <article>
-      <div style={{ width: "40vw" }}>
+    <Product>
+      <ImageContainer>
         <Carousel>
           {images.map(img => (
             <div>
@@ -21,25 +57,27 @@ const ProductDisplayPage: React.FC = () => {
             </div>
           ))}
         </Carousel>
-      </div>
-      <h1>{name}</h1>
-      <p>{description}</p>
-      <ContextConsumer>
-        {context => {
-          const [basket, updateBasket] = context
-          return <div>
-            <button onClick={() => {
-              updateBasket((prevBasket: IProduct[]) => {
-                return [...prevBasket, products[0]]
-              })
-            }}>
-              Add To Basket
-            </button>
-          </div>
-        }}
-      </ContextConsumer>
-      <h2>£{price}</h2>
-    </article>
+      </ImageContainer>
+      <ProductInfo>
+        <ImportantInfo>{name}</ImportantInfo>
+        <Description>{description}</Description>
+        <ContextConsumer>
+          {context => {
+            const [basket, updateBasket] = context
+            return (
+              <AddToBasket onClick={() => {
+                updateBasket((prevBasket: IProduct[]) => {
+                  return [...prevBasket, products[0]]
+                })
+              }}>
+                Add To Basket
+              </AddToBasket>
+            )
+          }}
+        </ContextConsumer>
+        <ImportantInfo>£{price}</ImportantInfo>
+      </ProductInfo>
+    </Product>
   );
 }
 
